9255b7cbfa036419c73291089f6ebfbce02da7b1
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2023-10-11 15:12:46 +0530

    Add files via upload

---
+++ DependencyInjectionEXP/DependencyInjectionEXP/EmployeeBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionEXP
+{
+    public class EmployeeBL
+    {
+        public IEmployeeDAL employeeDAL;
+       
+        public EmployeeBL(IEmployeeDAL employeeDAL)
+        { 
+            this.employeeDAL = employeeDAL;
+        }
+        public List<Employee>GetAllEmployees()
+        {
+          return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
+    
+
---
+++ DependencyInjectionEXP/DependencyInjectionEXP/EmployeeDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionEXP
+{
+    public class EmployeeDAL : IEmployeeDAL
+    {
+        public List<Employee> SelectAllEmployees()
+        {
+            List<Employee> ListEmployee = new List<Employee>();
+            {
+                new Employee() { Id = 1, Name = "Pranaya", Department = "IT" };
+                new Employee() { Id = 2, Name = "Kumar", Department = "HR" };
+                new Employee() { Id = 3, Name = "Rout", Department = "Payroll" };
+            };
+            return ListEmployee;
+        }
+    }
+}
